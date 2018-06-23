f20c704487563603f58c1cb2444d1d6d740a8c12
Nickwangpeng <nkwangpeng@126.com>
2018-06-23 12:21:31 +0800

    msic

--- 20180621-zdy-deal_data/txt2npz.sh
+++
@@ -1,8 +0,0 @@
-#########################################################################
-# File Name: txt2pkl.sh
-# Author: zhouduoyou
-# Mail: zhouduoyou@bytedance.com
-# Created Time: Thu 14 Jun 2018 08:20:56 PM CST
-#########################################################################
-#!/bin/bash
-python txt2npz.py --convert_dir ../factors/ --npzs_path ../processed_data4 --num_processes 10
